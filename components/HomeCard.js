63a43e29be61019e322703fa37f415e8a1746986
AlbusSong <strawberry1234561@gmail.com>
2022-02-07 18:08:36 +0200

    text-ellipsis overflow-hidden

--- components/HomeCard.js
+++ components/HomeCard.js
@@ -13,7 +13,7 @@ class HomeCard extends React.Component {
         const hrefPath = "/a/" + this.itemData.articleId;
         return (
             <div className='w-full px-2 py-2 break-inside-avoid-column' style={{  backgroundColor: `${white()}` }}>
-                <div className='w-full h-full px-3 py-3 rounded-md border-2 border-dashed shadow-md border-slate-200 bg-slate-50'>                    
+                <div className='w-full h-full px-3 py-3 rounded-md border-2 border-dashed shadow-md text-ellipsis overflow-hidden border-slate-200 bg-slate-50'>                    
                     <h2 className='text-black-200 text-3xl font-medium break-words'>
                         {this.itemData.zhTitle}
                     </h2>
