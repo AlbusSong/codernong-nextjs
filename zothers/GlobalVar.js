bce29ba776e3d14ebb74fb5c67011d418842c67f
AlbusSong <strawberry1234561@gmail.com>
2022-02-09 15:42:08 +0200

    URL_PREFIX

--- zothers/GlobalVar.js
+++ zothers/GlobalVar.js
@@ -1,4 +1,4 @@
-// const URL_PREFIX = "https://codernong.com:9002/";
-const URL_PREFIX = "http://127.0.0.1:9002/";
+const URL_PREFIX = "https://codernong.com:9002/";
+// const URL_PREFIX = "http://127.0.0.1:9002/";
 
 export { URL_PREFIX };
