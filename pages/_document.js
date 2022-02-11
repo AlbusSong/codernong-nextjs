0edb24ce3c31979e477c1f47cfb83295a83079db
AlbusSong <strawberry1234561@gmail.com>
2022-02-11 15:37:53 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,8 +10,6 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="shortcut icon" href="/geekrookie.png" />
-                    <script data-ad-client="ca-pub-8622528197740245" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
                     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8622528197740245"
                         crossOrigin="anonymous"></script>
                 </Head>
