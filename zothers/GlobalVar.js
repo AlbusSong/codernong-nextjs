8f7a865c975b68d2f566a9f941af5c4f1a17eea3
AlbusSong <strawberry1234561@gmail.com>
2022-05-05 00:10:18 +0300

    LOCAL_URL_PREFIX

--- zothers/GlobalVar.js
+++ zothers/GlobalVar.js
@@ -1,4 +1,6 @@
 const URL_PREFIX = "https://codernong.com:9001/";
 // const URL_PREFIX = "http://127.0.0.1:9001/";
 
-export { URL_PREFIX };
+const LOCAL_URL_PREFIX = "http://127.0.0.1:9001/";
+
+export { URL_PREFIX, LOCAL_URL_PREFIX };
